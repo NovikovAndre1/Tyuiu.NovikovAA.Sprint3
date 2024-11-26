ac4657be51cce0b4143c68b72688d6f5d0d066e3
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-11-26 18:02:14 +0600

    Task1

--- Tyuiu.NovikovAA.Sprint3.Task1.V21.Lib/DataService.cs
+++ Tyuiu.NovikovAA.Sprint3.Task1.V21.Lib/DataService.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.NovikovAA.Sprint3.Task1.V21.Lib
                 multSeries = multSeries * Math.Pow((0.5 + startValue / 5.0) / (Math.Cos(value) + 0.5), 3);
                 startValue++;
             }
-            return Math.Round(multSeries, 3);
+            return Math.Round(multSeries, 2);
         }
     }
 }
